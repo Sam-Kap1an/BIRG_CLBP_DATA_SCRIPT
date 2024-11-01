e1447b383e8fffbbf4daf96d97fee0a4d18cbebf
sfkaplan <sfkaplan@calpoly.edu>
2024-11-01 12:30:00 -0700

    done?

--- script.sh
+++ script.sh
@@ -1,29 +1,50 @@
 #!/bin/bash
+#!/bin/bash
 
 # Variables
 PYTHON_SCRIPT="script.py"  
-INPUT_FILE="CLBP_test_csv"              #  name for the input file w/o .csv extension
-OUTPUT_FILE="CLBP_test_csv_data.txt"        # name of the output text file
-REMOTE_USER="clbp"              
-REMOTE_HOST="birg.dev"     
-REMOTE_PATH="~/BIRG_CLBP_DATA_SCRIPT"      
+REMOTE_USER="clbp"                       
+REMOTE_HOST="birg.dev"                   
+REMOTE_PATH_TO_BIRG="BIRG_CLBP_DATA_SCRIPT" # path to save output on remote server
+
+# Prompt for necessary paths and input
+echo "Enter the local path to the current directory (where your script and input file are located):"
+read LOCAL_PATH
+
+echo "Enter the path to the SSH private key file:"
+read REMOTE_PATH_TO_SSHKEY
+
+echo "Enter the full path to the directory of the input CSV file:"
+read LOCAL_INPUT_PATH
+
+
+echo "Enter the  name of the input CSV file:"
+read INPUT_NAME
+
+# Full paths for script and output
+LOCAL_SCRIPT_PATH="$LOCAL_PATH/$PYTHON_SCRIPT"
+LOCAL_OUTPUT_PATH="$LOCAL_PATH/$OUTPUT_FILE"
+
+
+OUTPUT_FILE="$INPUT_NAME""_data.txt"     # name of the output text file
 
 # Run the Python script with the input and output file names as arguments
-python3 "$REMOTE_PATH$PYTHON_SCRIPT" "$REMOTE_PATH$INPUT_FILE" "$REMOTE_PATH$OUTPUT_FILE"
+python3 "$LOCAL_SCRIPT_PATH" "$LOCAL_INPUT_PATH/$INPUT_NAME" "$LOCAL_OUTPUT_PATH"
 
 # Check if Python script ran successfully
 if [ $? -eq 0 ]; then
     echo "Python script ran successfully. Output saved to $OUTPUT_FILE."
 
-    # Copy the text file to the remote server
-    scp  -i ~./ssh/clbp_birg_key "$OUTPUT_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+    # Copy the output text file to the remote server
+    scp -i "$REMOTE_PATH_TO_SSHKEY" "$LOCAL_OUTPUT_PATH" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH_TO_BIRG/$OUTPUT_FILE"
 
     # Check if scp was successful
     if [ $? -eq 0 ]; then
-        echo "File successfully copied to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH."
+        echo "File successfully copied to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH_TO_BIRG."
     else
         echo "Error: Failed to copy the file to the remote server."
     fi
 else
     echo "Error: Python script did not run successfully."
 fi
+
