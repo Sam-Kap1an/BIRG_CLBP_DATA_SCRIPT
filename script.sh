b0aa679617b054362abcd3d7d9918e1dc89f7764
Paul Anderson <pauleanderson@gmail.com>
2024-11-06 09:53:05 -0800

    update

--- script.sh
+++ script.sh
@@ -9,15 +9,19 @@ REMOTE_PATH_TO_BIRG="BIRG_CLBP_DATA_SCRIPT"  # Path to save output on remote ser
 # Prompt for necessary paths and input
 echo "Enter the local path to the current directory (where your script and input file are located):"
 read LOCAL_PATH
+LOCAL_PATH=$(echo $LOCAL_PATH|tr -d '\r')
 
 echo "Enter the path to the SSH private key file:"
 read REMOTE_PATH_TO_SSHKEY
+REMOTE_PATH_TO_SSHKEY=$(echo $REMOTE_PATH_TO_SSHKEY|tr -d '\r')
 
 echo "Enter the full path to the directory of the input CSV file:"
 read LOCAL_INPUT_PATH
+LOCAL_INPUT_PATH=$(echo $LOCAL_INPUT_PATH|tr -d '\r')
 
 echo "Enter the name of the input CSV file:"
 read INPUT_NAME
+INPUT_NAME=$(echo $INPUT_NAME|tr -d '\r')
 
 # Define the output file name
 OUTPUT_FILE="${INPUT_NAME}_data.txt"
@@ -28,7 +32,7 @@ LOCAL_INPUT_FILE="$LOCAL_INPUT_PATH/$INPUT_NAME"
 LOCAL_OUTPUT_PATH="$LOCAL_PATH/$OUTPUT_FILE"
 
 # Run the Python script locally with the input and output files as arguments
-python3 "$LOCAL_SCRIPT_PATH" "$LOCAL_INPUT_FILE" "$LOCAL_OUTPUT_PATH"
+python "$LOCAL_SCRIPT_PATH" "$LOCAL_INPUT_FILE" "$LOCAL_OUTPUT_PATH"
 
 # Check if Python script ran successfully
 if [ $? -eq 0 ]; then
